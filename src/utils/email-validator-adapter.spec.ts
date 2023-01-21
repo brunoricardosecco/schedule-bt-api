02f520b0ce108fdf2b0eaac39d0bd79e30af5905
bsecco <bsecco@shutterstock.com>
2023-01-21 16:21:56 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -19,8 +19,17 @@ describe('EmailValidatorAdapter', () => {
 
   it('should returns true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
+  })
+
+  it('should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
