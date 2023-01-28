aaa647b610509420f7353f5d0c173b7912ed8c82
bsecco <bsecco@shutterstock.com>
2023-01-28 15:45:01 -0300

    chore: remove protocols from tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 const config = {
   roots: ['<rootDir>/src'],
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!**/*.protocols.ts',
+    '!**/protocols/**'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
