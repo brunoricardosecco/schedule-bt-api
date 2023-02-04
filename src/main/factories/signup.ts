c6ca4973c5114f7cc0e80418624d32c46e13fb82
bsecco <bsecco@shutterstock.com>
2023-02-04 14:18:49 -0300

    feat: add LogPostgresRepository to the SignUp factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/AddAccount/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountPostgresRepository } from '../../infra/db/postgres/account-repository/account'
+import { LogPostgresRepository } from '../../infra/db/postgres/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -13,6 +14,7 @@ export const makeSignUpController = (): Controller => {
   const accountPostgresRepository = new AccountPostgresRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountPostgresRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const logPostgresRepository = new LogPostgresRepository()
 
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logPostgresRepository)
 }
