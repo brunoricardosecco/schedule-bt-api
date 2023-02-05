6a9ec9da5d32a76bd377480716575b3a015f2d60
bsecco <bsecco@shutterstock.com>
2023-02-05 11:22:24 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('RequireField Validation', () => {
+  it('should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'different_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  it('should return null if validation succeeds', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
