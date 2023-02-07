5d77caacca3055fb64695dc2049d5127bb73d430
bsecco <bsecco@shutterstock.com>
2023-02-07 19:42:26 -0300

    test: ensure AccountPostgresRepository returns an account on loadByEmail success

--- src/infra/db/postgres/account-repository/account.spec.ts
+++ src/infra/db/postgres/account-repository/account.spec.ts
@@ -21,7 +21,7 @@ describe('Account Postgres Repository', () => {
     ])
   })
 
-  it('should return an account on success', async () => {
+  it('should return an account on add success', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
@@ -34,5 +34,23 @@ describe('Account Postgres Repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  it('should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await db.accounts.create({
+      data: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password'
+      }
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
+
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account?.name).toBe('any_name')
+    expect(account?.email).toBe('any_email@mail.com')
+    expect(account?.password).toBe('any_password')
   })
 })
