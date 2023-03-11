85ad2d2055f5e75c093a6d2e5eba864ec141719c
bsecco <bsecco@shutterstock.com>
2023-03-11 17:45:36 -0300

    ✨ feat(serviceHour.ts): add ServiceHour model with id, weekday, startTime, endTime, companyId, and company properties

---
+++ src/domain/models/serviceHour.ts
@@ -0,0 +1,10 @@
+import { Company } from './company'
+
+export type ServiceHour = {
+  id: string
+  weekday: number
+  startTime: string
+  endTime: string
+  companyId: string
+  company: Company
+}
