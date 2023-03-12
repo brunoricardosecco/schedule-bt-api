6280d8a2a0c8d784a2b3143142292eebcebad952
bsecco <bsecco@shutterstock.com>
2023-03-12 10:49:49 -0300

    test: ensure DbAddCompanyServiceHour calls DbAddCompanyServiceHourRepository with correct values

---
+++ src/data/usecases/add-company-service-hour/db-add-company-service-hour.spec.ts
@@ -0,0 +1,62 @@
+import { AddCompanyServiceHourRepository, AddCompanyServiceHourRepositoryModel } from '@/data/protocols/db/service-hour/add-company-service-hour'
+import { ServiceHour } from '@/domain/models/serviceHour'
+import { AddCompanyServiceHour } from '@/domain/usecases/add-company-service-hour'
+import { Company } from '../add-company/db-add-company.protocols'
+import { DbAddCompanyServiceHour } from './db-add-company-service-hour'
+
+const makeFakeCompany = (): Company => ({
+  id: 'valid_id',
+  name: 'verona',
+  reservationPrice: 60,
+  reservationTimeInMinutes: 60,
+  createdAt: new Date(),
+  updatedAt: new Date()
+})
+
+const makeFakeServiceHour = (): ServiceHour => ({
+  id: 'valid_id',
+  company: makeFakeCompany(),
+  companyId: 'company_id',
+  startTime: '09:00',
+  endTime: '22:00',
+  weekday: 0
+})
+
+const makeFakeServiceHourData = (): AddCompanyServiceHourRepositoryModel => ({
+  weekday: 0,
+  startTime: '09:00',
+  endTime: '22:00',
+  companyId: 'company_id'
+})
+
+class AddCompanyServiceHourRepositoryStub implements AddCompanyServiceHourRepository {
+  async add (serviceHourData: AddCompanyServiceHourRepositoryModel): Promise<ServiceHour> {
+    return await new Promise(resolve => { resolve(makeFakeServiceHour()) })
+  }
+}
+
+type SutTypes = {
+  addCompanyRepository: AddCompanyServiceHourRepository
+  sut: AddCompanyServiceHour
+}
+
+const makeSut = (): SutTypes => {
+  const addCompanyRepository = new AddCompanyServiceHourRepositoryStub()
+  const sut = new DbAddCompanyServiceHour(addCompanyRepository)
+
+  return {
+    addCompanyRepository,
+    sut
+  }
+}
+
+describe('DbAddCompanyServiceHour Usecase', () => {
+  it('should call AddCompanyServiceHourRepository with correct values', async () => {
+    const { addCompanyRepository, sut } = makeSut()
+
+    const addSpy = jest.spyOn(addCompanyRepository, 'add')
+    await sut.add(makeFakeServiceHourData())
+
+    expect(addSpy).toHaveBeenCalledWith(makeFakeServiceHourData())
+  })
+})
