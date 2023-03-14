a61c4b6998a9aa70e2a278ce44c4b59c27daf4f3
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2023-03-14 00:01:33 -0300

    feat: add auth middleware

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,16 +1,38 @@
+import { Authenticate } from '@/data/usecases/authenticate/authenticate'
 import { AccessDeniedError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/httpHelper'
+import { forbidden, serverError } from '@/presentation/helpers/http/httpHelper'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { Middleware } from '@/presentation/protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly authenticate: Authenticate
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.headers?.Authorization) {
-      return forbidden(new AccessDeniedError())
-    }
-    return {
-      body: {},
-      statusCode: 200
+    try {
+      if (!httpRequest.headers?.Authorization) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      const [, token] = httpRequest.headers.Authorization.split('Bearer ')
+
+      if (!token) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      const payloadOrError = await this.authenticate.auth(token)
+
+      if (payloadOrError instanceof Error) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      return {
+        body: payloadOrError,
+        statusCode: 200
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
