4724a64e8df61b69d71e72e02bb1d43f87ef7b22
bsecco <bsecco@shutterstock.com>
2023-03-23 20:22:18 -0300

    feat: ensure ServiceHuorPostgresRepository add a service hour on success

--- src/infra/db/postgres/service-hour/service-hour-postgres-repository.ts
+++ src/infra/db/postgres/service-hour/service-hour-postgres-repository.ts
@@ -1,15 +1,10 @@
 import { AddServiceHourRepository, AddServiceHourRepositoryModel, ServiceHour } from '@/data/usecases/add-company-service-hour/db-add-service-hour.protocols'
+import { db } from '@/infra/db/orm/prisma'
 
 export class ServiceHourPostgresRepository implements AddServiceHourRepository {
   async add (serviceHourData: AddServiceHourRepositoryModel): Promise<ServiceHour> {
-    return await new Promise(resolve => {
-      resolve({
-        weekday: 0,
-        startTime: '09:00',
-        endTime: '20:00',
-        companyId: 'ss',
-        id: 'aa'
-      })
+    return await db.serviceHours.create({
+      data: serviceHourData
     })
   }
 }
