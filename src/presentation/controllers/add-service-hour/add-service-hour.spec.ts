8534de9a77466b62c5111f6faedd91a6f2058076
bsecco <bsecco@shutterstock.com>
2023-03-24 18:12:57 -0300

    test: ensure AddServiceHourController calls AddServiceHour with correct values

---
+++ src/presentation/controllers/add-service-hour/add-service-hour.spec.ts
@@ -0,0 +1,65 @@
+import { ServiceHour } from '@/domain/models/serviceHour'
+import { AddServiceHour, AddServiceHourModel } from '@/domain/usecases/add-service-hour'
+import { HttpRequest } from '@/presentation/protocols'
+import { AddServiceHourController } from './add-service-hour-controller'
+
+const makeAddServiceHourModel = (): ServiceHour => ({
+  companyId: 'any_company_id',
+  endTime: 'any_start_time',
+  startTime: 'any_end_time',
+  weekday: 0,
+  id: 'any_id'
+})
+
+const makeAddServiceHour = (): AddServiceHour => {
+  class AddServiceHourStub implements AddServiceHour {
+    async add (serviceHour: AddServiceHourModel): Promise<ServiceHour | Error> {
+      return await new Promise(resolve => { resolve(makeAddServiceHourModel()) })
+    }
+  }
+
+  return new AddServiceHourStub()
+}
+
+const makeFakeServiceHourData = (): AddServiceHourModel => ({
+  companyId: 'any_company_id',
+  endTime: 'any_start_time',
+  startTime: 'any_end_time',
+  weekday: 0
+})
+
+const addServiceHourData = makeFakeServiceHourData()
+
+const makeFakeRequest = (): HttpRequest => {
+  return {
+    body: addServiceHourData
+  }
+}
+
+type SutTypes = {
+  addServiceHour: AddServiceHour
+  sut: AddServiceHourController
+}
+
+const makeSut = (): SutTypes => {
+  const addServiceHour = makeAddServiceHour()
+
+  const sut = new AddServiceHourController(addServiceHour)
+
+  return {
+    addServiceHour,
+    sut
+  }
+}
+
+describe('AddServiceHour Controller', () => {
+  it('should call AddServiceHour with correct values', async () => {
+    const { addServiceHour, sut } = makeSut()
+
+    const addSpy = jest.spyOn(addServiceHour, 'add')
+
+    await sut.handle(makeFakeRequest())
+
+    expect(addSpy).toHaveBeenCalledWith(addServiceHourData)
+  })
+})
