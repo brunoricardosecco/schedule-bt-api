e3cf98f319727768636e92051060fed10be0390d
bsecco <bsecco@shutterstock.com>
2023-03-24 19:36:11 -0300

    feat: ensure makeAddServiceHourValidation calls ValidationComposite with all validations

---
+++ src/main/factories/controllers/add-service-hour/add-service-hour-validation-factory.ts
@@ -0,0 +1,12 @@
+import { Validation } from '@/presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+
+export const makeAddServiceHourValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+
+  for (const field of ['weekday', 'startTime', 'endTime', 'companyId']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}
