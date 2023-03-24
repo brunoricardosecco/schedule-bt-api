6788c50977bbdb2bf76ad998a2c74cfec1a40b9c
bsecco <bsecco@shutterstock.com>
2023-03-24 19:18:00 -0300

    feat: ensure AddServiceHourController calls Validation with correct values

--- src/presentation/controllers/add-service-hour/add-service-hour-controller.ts
+++ src/presentation/controllers/add-service-hour/add-service-hour-controller.ts
@@ -1,14 +1,17 @@
 import { AddServiceHour } from '@/domain/usecases/add-service-hour'
 import { ok, serverError } from '@/presentation/helpers/http/httpHelper'
-import { Controller, HttpRequest, HttpResponse } from '../add-company/add-company-controller.protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../add-company/add-company-controller.protocols'
 
 export class AddServiceHourController implements Controller {
   constructor (
-    private readonly addServiceHour: AddServiceHour
+    private readonly addServiceHour: AddServiceHour,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
+
       const serviceHour = await this.addServiceHour.add(httpRequest.body)
 
       return ok({
