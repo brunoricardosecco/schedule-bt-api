e84cdfc43a088ff2fb85f2278f95ed5842bf7f45
bsecco <bsecco@shutterstock.com>
2023-03-24 19:18:27 -0300

    test: ensure AddServiceHourController calls Validation with correct values

--- src/presentation/controllers/add-service-hour/add-service-hour.spec.ts
+++ src/presentation/controllers/add-service-hour/add-service-hour.spec.ts
@@ -2,7 +2,7 @@ import { ServiceHour } from '@/domain/models/serviceHour'
 import { AddServiceHour, AddServiceHourModel } from '@/domain/usecases/add-service-hour'
 import { ServerError } from '@/presentation/errors'
 import { ok, serverError } from '@/presentation/helpers/http/httpHelper'
-import { HttpRequest } from '@/presentation/protocols'
+import { HttpRequest, Validation } from '@/presentation/protocols'
 import { AddServiceHourController } from './add-service-hour-controller'
 
 const makeFakeServiceHour = (): ServiceHour => ({
@@ -23,6 +23,16 @@ const makeAddServiceHour = (): AddServiceHour => {
   return new AddServiceHourStub()
 }
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
 const makeFakeServiceHourData = (): AddServiceHourModel => ({
   companyId: 'any_company_id',
   endTime: 'any_start_time',
@@ -40,39 +50,41 @@ const makeFakeRequest = (): HttpRequest => {
 }
 
 type SutTypes = {
-  addServiceHour: AddServiceHour
+  validationStub: Validation
+  addServiceHourStub: AddServiceHour
   sut: AddServiceHourController
 }
 
 const makeSut = (): SutTypes => {
-  const addServiceHour = makeAddServiceHour()
+  const addServiceHourStub = makeAddServiceHour()
+  const validationStub = makeValidation()
 
-  const sut = new AddServiceHourController(addServiceHour)
+  const sut = new AddServiceHourController(addServiceHourStub, validationStub)
 
   return {
-    addServiceHour,
+    validationStub,
+    addServiceHourStub,
     sut
   }
 }
 
 describe('AddServiceHour Controller', () => {
   it('should call AddServiceHour with correct values', async () => {
-    const { addServiceHour, sut } = makeSut()
+    const { addServiceHourStub, sut } = makeSut()
 
-    const addSpy = jest.spyOn(addServiceHour, 'add')
+    const addSpy = jest.spyOn(addServiceHourStub, 'add')
 
     await sut.handle(makeFakeRequest())
 
     expect(addSpy).toHaveBeenCalledWith(addServiceHourData)
   })
   it('should return 500 if AddServiceHour throws', async () => {
-    const { sut, addServiceHour } = makeSut()
-    jest.spyOn(addServiceHour, 'add').mockImplementationOnce(async () => { return await new Promise((resolve, reject) => { reject(new Error()) }) })
+    const { sut, addServiceHourStub } = makeSut()
+    jest.spyOn(addServiceHourStub, 'add').mockImplementationOnce(async () => { return await new Promise((resolve, reject) => { reject(new Error()) }) })
 
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new ServerError()))
   })
-
   it('should return 200 if a valid data is provided', async () => {
     const { sut } = makeSut()
 
@@ -81,5 +93,16 @@ describe('AddServiceHour Controller', () => {
     expect(httpResponse).toEqual(ok({
       serviceHour
     }))
+  })
+  it('should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+
+    const httpRequest = makeFakeRequest()
+
+    await sut.handle(makeFakeRequest())
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
