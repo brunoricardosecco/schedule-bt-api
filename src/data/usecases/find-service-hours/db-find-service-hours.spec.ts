5fc84e5c0dd26fbbc09c64052d963174409c4405
bsecco <bsecco@shutterstock.com>
2023-03-25 17:06:34 -0300

    test: ensure DbFindServiceHour calls FindServiceHoursRepository with correct values

---
+++ src/data/usecases/find-service-hours/db-find-service-hours.spec.ts
@@ -0,0 +1,57 @@
+import { FindServiceHoursRepository, FindServiceHoursRepositoryParams } from '@/data/protocols/db/service-hour/find-service-hours'
+import { ServiceHour } from '../add-service-hour/db-add-service-hour.protocols'
+import { DbFindServiceHours } from './db-find-service-hours'
+
+const makeFakeServiceHour = (): ServiceHour => ({
+  id: 'valid_id',
+  companyId: 'company_id',
+  startTime: '09:00',
+  endTime: '12:00',
+  weekday: 0
+})
+
+const fakeServiceHour = makeFakeServiceHour()
+
+const makeFindServiceHoursRepository = (): FindServiceHoursRepository => {
+  class FindServiceHoursRepositoryStub implements FindServiceHoursRepository {
+    async findBy (params: FindServiceHoursRepositoryParams): Promise<ServiceHour[]> {
+      return await new Promise(resolve => { resolve([fakeServiceHour]) })
+    }
+  }
+
+  return new FindServiceHoursRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbFindServiceHours
+  findServiceHoursRepository: FindServiceHoursRepository
+}
+
+const makeSut = (): SutTypes => {
+  const findServiceHoursRepositoryStub = makeFindServiceHoursRepository()
+  const sut = new DbFindServiceHours(findServiceHoursRepositoryStub)
+
+  return {
+    sut,
+    findServiceHoursRepository: findServiceHoursRepositoryStub
+  }
+}
+
+describe('DbFindServiceHours', () => {
+  it('should returns service hours on success', async () => {
+    const { sut, findServiceHoursRepository } = makeSut()
+
+    const findBySpy = jest.spyOn(findServiceHoursRepository, 'findBy')
+
+    const params = {
+      companyId: 'any_id',
+      weekday: 0
+    }
+
+    const serviceHours = await sut.find(params)
+
+    expect(findBySpy).toHaveBeenCalledWith(params)
+    expect(serviceHours.length).toBeGreaterThan(0)
+    expect(serviceHours[0]).toEqual(fakeServiceHour)
+  })
+})
