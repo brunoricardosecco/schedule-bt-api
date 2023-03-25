90a5f3e5c03c20447eba6d15054b6585e01e0c7e
bsecco <bsecco@shutterstock.com>
2023-03-25 17:58:35 -0300

    feat: ensure ServiceHourPostgresRepositor's findby retuns correctly on success

--- src/infra/db/postgres/service-hour/service-hour-postgres-repository.ts
+++ src/infra/db/postgres/service-hour/service-hour-postgres-repository.ts
@@ -1,8 +1,11 @@
+import { FindServiceHoursRepository, FindServiceHoursRepositoryParams } from '@/data/protocols/db/service-hour/find-service-hours'
 import { LoadServiceHoursByCompanyIdAndWeekdayRepository, LoadServiceHoursByCompanyIdRepositoryModel } from '@/data/protocols/db/service-hour/load-service-hours-by-company-id-and-weekday'
 import { AddServiceHourRepository, AddServiceHourRepositoryModel, ServiceHour } from '@/data/usecases/add-service-hour/db-add-service-hour.protocols'
 import { db } from '@/infra/db/orm/prisma'
+import { } from '@prisma/client'
+// import { composeFilters } from '@/infra/db/utils/compose-filters'
 
-export class ServiceHourPostgresRepository implements AddServiceHourRepository, LoadServiceHoursByCompanyIdAndWeekdayRepository {
+export class ServiceHourPostgresRepository implements AddServiceHourRepository, LoadServiceHoursByCompanyIdAndWeekdayRepository, FindServiceHoursRepository {
   async add (serviceHourData: AddServiceHourRepositoryModel): Promise<ServiceHour> {
     return await db.serviceHours.create({
       data: serviceHourData
@@ -15,6 +18,12 @@ export class ServiceHourPostgresRepository implements AddServiceHourRepository, 
         companyId,
         weekday
       }
+    })
+  }
+
+  async findBy (params: FindServiceHoursRepositoryParams): Promise<ServiceHour[]> {
+    return await db.serviceHours.findMany({
+      where: params
     })
   }
 }
