463f6daea92a0830c8abc35aaebdd5a04f6cab59
bsecco <bsecco@shutterstock.com>
2023-03-26 14:49:57 -0300

    test: ensure DeleteServiceHour returns a the deleted service hour

---
+++ src/data/usecases/delete-service-hour/delete-service-hour.spec.ts
@@ -0,0 +1,52 @@
+import { DeleteServiceHourRepository } from '@/data/protocols/db/service-hour/delete-service-hour'
+import { IDeleteServiceHour } from '@/domain/usecases/delete-service-hour'
+import { ServiceHour } from '../add-service-hour/db-add-service-hour.protocols'
+import { DeleteServiceHour } from './delete-service-hour'
+
+const makeFakeServiceHour = (): ServiceHour => ({
+  id: 'valid_id',
+  companyId: 'company_id',
+  startTime: '09:00',
+  endTime: '12:00',
+  weekday: 0
+})
+
+const fakeServiceHour = makeFakeServiceHour()
+
+const makeDeleteServiceHourRepository = (): DeleteServiceHourRepository => {
+  class DeleteServiceHourRepositoryStub implements DeleteServiceHourRepository {
+    async delete (serviceHourId: string): Promise<ServiceHour> {
+      return await new Promise(resolve => { resolve(fakeServiceHour) })
+    }
+  }
+
+  return new DeleteServiceHourRepositoryStub()
+}
+
+type SutTypes = {
+  sut: IDeleteServiceHour
+  deleteServiceHourRepository: DeleteServiceHourRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteServiceHourRepository = makeDeleteServiceHourRepository()
+  const sut = new DeleteServiceHour(deleteServiceHourRepository)
+
+  return {
+    deleteServiceHourRepository,
+    sut
+  }
+}
+
+describe('DbDeleteServiceHour', () => {
+  it('should return null on success', async () => {
+    const { deleteServiceHourRepository, sut } = makeSut()
+    const deleteSpy = jest.spyOn(deleteServiceHourRepository, 'delete')
+    const serviceHourId = 'any_id'
+
+    const serviceHour = await sut.delete(serviceHourId)
+
+    expect(deleteSpy).toHaveBeenCalledWith(serviceHourId)
+    expect(serviceHour).toEqual(fakeServiceHour)
+  })
+})
