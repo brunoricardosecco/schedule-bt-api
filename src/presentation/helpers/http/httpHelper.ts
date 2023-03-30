7e6e65265257b80a6facf73501df1752a0be2b03
intwone <cassiointw1993@gmail.com>
2023-03-30 09:36:39 -0300

    feat: add new status code on httpHelper and add notFoundError

--- src/presentation/helpers/http/httpHelper.ts
+++ src/presentation/helpers/http/httpHelper.ts
@@ -1,20 +1,21 @@
 import { ServerError } from '@/presentation/errors'
+import { NotFoundError } from '@/presentation/errors/not-found-error'
 import { UnauthorizedError } from '@/presentation/errors/unauthorized-error'
 import { HttpResponse } from '@/presentation/protocols'
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
-
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: new ServerError(error.stack)
-})
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
 })
 
 export const unauthorized = (): HttpResponse => ({
@@ -26,3 +27,19 @@ export const forbidden = (error: Error): HttpResponse => ({
   statusCode: 403,
   body: error
 })
+
+export const notFound = (): HttpResponse => ({
+  statusCode: 404,
+  body: new NotFoundError()
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+
+
+
+
+
