3b054ec25f51337a2c39bef0d18021e410ae8ca0
bsecco <bsecco@shutterstock.com>
2023-04-01 16:23:33 -0300

    test: ensure FindCompanesRepostory returns companies for a given filter

--- src/infra/db/postgres/company/company-postgres-repository.spec.ts
+++ src/infra/db/postgres/company/company-postgres-repository.spec.ts
@@ -10,19 +10,50 @@ describe('Company Postgres Repository', () => {
     await db.$executeRawUnsafe('TRUNCATE TABLE "companies" CASCADE;')
   })
 
-  it('should return an company on add success', async () => {
-    const sut = makeSut()
-    const params = {
-      name: 'any_name',
-      reservationPrice: 60,
-      reservationTimeInMinutes: 80
-    }
-    const company = await sut.add(params)
+  describe('add', () => {
+    it('should return an company on add success', async () => {
+      const sut = makeSut()
+      const params = {
+        name: 'any_name',
+        reservationPrice: 60,
+        reservationTimeInMinutes: 80
+      }
+      const company = await sut.add(params)
 
-    expect(company).toBeTruthy()
-    expect(company.id).toBeTruthy()
-    expect(company.name).toBe(params.name)
-    expect(company.reservationPrice).toBe(params.reservationPrice)
-    expect(company.reservationTimeInMinutes).toBe(params.reservationTimeInMinutes)
+      expect(company).toBeTruthy()
+      expect(company.id).toBeTruthy()
+      expect(company.name).toBe(params.name)
+      expect(company.reservationPrice).toBe(params.reservationPrice)
+      expect(company.reservationTimeInMinutes).toBe(params.reservationTimeInMinutes)
+    })
+  })
+  describe('findBy', () => {
+    it('should return company list for a given filter on success', async () => {
+      const sut = makeSut()
+
+      const createdCompany = await db.companies.create({
+        data: {
+          name: 'any_name',
+          reservationPrice: 1000,
+          reservationTimeInMinutes: 60
+        }
+      })
+
+      const params = {
+        companyId: createdCompany.id
+      }
+
+      const companies = await sut.findBy(params)
+
+      expect(companies.length).toEqual(1)
+      expect(companies[0]).toEqual({
+        id: expect.any(String),
+        name: 'any_name',
+        reservationPrice: 1000,
+        reservationTimeInMinutes: 60,
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date)
+      })
+    })
   })
 })
