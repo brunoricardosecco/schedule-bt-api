40246729eeefd9ae50c41e94649be7af6757e6f6
bsecco <bsecco@shutterstock.com>
2023-04-01 16:47:27 -0300

    feat: add FindReservationIntervalsFactory

---
+++ src/main/factories/usecases/find-reservation-intervals/find-reservation-intervals-factory.ts
@@ -0,0 +1,13 @@
+import { FindReservationIntervals } from '@/data/usecases/find-reservation-intervals/find-reservation-intervals'
+import { IFindReservationIntervals } from '@/domain/usecases/find-reservation-intervals'
+import { CompanyPostgresRepository } from '@/infra/db/postgres/company/company-postgres-repository'
+import { ReservationPostgresRepository } from '@/infra/db/postgres/reservation/reservation-postgres-repository'
+import { ServiceHourPostgresRepository } from '@/infra/db/postgres/service-hour/service-hour-postgres-repository'
+
+export const makeReservationIntervals = (): IFindReservationIntervals => {
+  const serviceHourPostgresRepository = new ServiceHourPostgresRepository()
+  const companyPostgresRepository = new CompanyPostgresRepository()
+  const reservationPostgresRepository = new ReservationPostgresRepository()
+
+  return new FindReservationIntervals(serviceHourPostgresRepository, companyPostgresRepository, reservationPostgresRepository)
+}
