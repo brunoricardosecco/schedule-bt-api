ae7d9fecfa45c0b7045b5e74a7be083a5ff9e2f3
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2023-04-11 15:55:12 -0300

    chore: add graceful shutdown on app (#6)

---
+++ src/main/config/graceful-shutdown.ts
@@ -0,0 +1,23 @@
+import { Server } from 'http'
+
+enum ExitStatus {
+  FAILURE = 1,
+  SUCCESS = 0,
+}
+
+export default (server: Server): void => {
+  const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+
+  for (const exitSignal of exitSignals) {
+    process.on(exitSignal, async () => {
+      try {
+        server.close()
+        console.log('App exited with success')
+        process.exit(ExitStatus.SUCCESS)
+      } catch (error) {
+        console.error(`App exited with error: ${error}`)
+        process.exit(ExitStatus.FAILURE)
+      }
+    })
+  }
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,14 @@
 import { config } from 'dotenv'
 import 'module-alias/register'
 import app from './config/app'
+import gracefulShutdown from './config/graceful-shutdown'
 
 config()
 
 const port = process.env.PORT ?? 3000
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on localhost:${port}`)
 })
+
+gracefulShutdown(server)
